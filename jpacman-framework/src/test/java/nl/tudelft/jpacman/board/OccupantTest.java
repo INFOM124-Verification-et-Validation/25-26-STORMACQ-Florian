3293856f4079b23c6a86be50adef56864269cc43
FloSto-Bash <florian_stormacq@icloud.com>
2025-10-14 08:54:11 +0200

    Add TP1

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
@@ -31,7 +31,7 @@ class OccupantTest {
      */
     @Test
     void noStartSquare() {
-        // TODO
+        assertThat(unit.hasSquare()).isFalse();
     }
 
     /**
@@ -40,7 +40,9 @@ class OccupantTest {
      */
     @Test
     void testOccupy() {
-        // TODO
+        Square square = new BasicSquare();
+        unit.occupy(square);
+        assertThat(unit.getSquare()).isEqualTo(square);
     }
 
     /**
@@ -49,6 +51,12 @@ class OccupantTest {
      */
     @Test
     void testReoccupy() {
-        // TODO
+        Square firstSquare = new BasicSquare();
+        Square secondSquare = new BasicSquare();
+        unit.occupy(firstSquare);
+        unit.occupy(secondSquare);
+        assertThat(unit.getSquare()).isEqualTo(secondSquare);
+
+        // assert unit.gestSquare() == second
     }
 }
